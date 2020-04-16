78cc3b85ac649d8b280a121c55006bb4ad692e14
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-16 14:53:47 -0400

    receive_sms.py Updated

--- receive_sms.py
+++ receive_sms.py
@@ -1,14 +1,30 @@
 # Import the flask framework
 from flask import Flask
 
+# Class in twillion that allows us to response to the text messages when someone text to our twilio phone number
+# This is needed to responde to any texts the come in
+from twilio.twiml.messaging_response import MessagingResponse
+
 # Create our flask app
 web_app = Flask(__name__)
 
 #
 def sms_reply():
-    return "This is an autoreply from Python 🐍 and Twilio 🟪."
+
+    # Create a response object (translates string to twiml for us. Twilio excpects twiml.)
+    automatic_response = MessagingResponse()
+
+    # Put a message in our response object
+    automatic_response.message("This is an autoreply from Python 🐍 and Twilio 🟪.")
+    pritn(automatic_response)
+
+    # Return message to our flask website
+    return str(automatic_response)
 
 # Linking a specific url to function
 web_app.add_url_rule('/sms', 'sms_reply', sms_reply)   # When ew get to '/' url call flask_url function
 
-web_app.run()
+# Code starts here
+if __name__ == '__main__':   # If we run this from commandline, then run the app
+    # Run this flask app on our local server 5000
+    web_app.run()
