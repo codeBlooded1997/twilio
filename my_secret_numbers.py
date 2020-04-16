a78216d96d77f4534e9c9a8f2b142c2ec6d0628b
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-16 13:13:14 -0400

    send_sms.py Updated
    app updated and tested, It's perfectly working.

--- my_secret_numbers.py
+++ my_secret_numbers.py
@@ -1,6 +1,2 @@
-# Import twilio
-# Client creates a Client object in our python app to let us communicte with client
-from twilio.rest import Client
-
-
-from my_secret_numbers import my_auth_token, my_cell_number
+my_cell_number = "+14387004763"
+my_auth_token =  "215fd27873ba3e867205e709e1634a6c"
--- send_sms.py
+++ send_sms.py
@@ -1,2 +1,23 @@
-my_cell_number = "+14387004763"
-my_auth_token =  "215fd27873ba3e867205e709e1634a6c"
+# Import twilio
+# Client creates a Client object in our python app to let us communicte with client
+from twilio.rest import Client
+
+#
+from my_secret_numbers import my_auth_token, my_cell_number
+
+# Codes required by twiliio. These are specific to the twilio account being used.
+account_sid = "AC81b4362f605c6398a48da412dcff6926"
+auth_token = my_auth_token
+
+# Make our twilio Client (Unoque to out account)
+client = Client(account_sid, auth_token)
+
+#
+sender = "+16194326457"
+receiver = my_cell_number
+
+# The text that we will text out
+text = "Yo, wassup."
+
+# Use the client to send the SMS text
+client.messages.create(to=receiver, from_=sender, body=text) # create method, creates and sends sms in one step
