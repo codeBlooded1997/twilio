51fb21c7389f5d5a25d1cfa5f9370665006bb608
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-16 14:03:54 -0400

    Udpated receive_sms.py

--- receive_sms.py
+++ receive_sms.py
@@ -1,1 +1,14 @@
+# Import the flask framework
 from flask import Flask
+
+# Create our flask app
+web_app = Flask(__name__)
+
+#
+def flask_url():
+    return "Hello"
+
+# Linking a specific url to function
+web_app.add_url_rule('/Y', 'flask_url', flask_url)   # When ew get to '/' url call flask_url function
+
+web_app.run()
