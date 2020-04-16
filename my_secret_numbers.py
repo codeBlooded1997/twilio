ec546a314f69cd58958f2b21122a508de9b941a9
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-04-16 12:59:24 -0400

    send_sms.py Updated

---
+++ my_secret_numbers.py
@@ -0,0 +1,6 @@
+# Import twilio
+# Client creates a Client object in our python app to let us communicte with client
+from twilio.rest import Client
+
+
+from my_secret_numbers import my_auth_token, my_cell_number
---
+++ send_sms.py
@@ -0,0 +1,2 @@
+my_cell_number = "+14387004763"
+my_auth_token =  "215fd27873ba3e867205e709e1634a6c"
